a9c6f549789ba4803fa2d72839ac298ff02285ab
KateGEVH <kategustilo@mac.com>
2021-08-25 12:47:59 -0600

    Updates

---
+++ Assets/KateScripts/ClicktestScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClicktestScript : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+
+        Debug.Log("The sphere has been clicked");
+    }
+
+}
